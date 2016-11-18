0211e94e141b9e57d1affc476b3e3b3f8d3add28
jrmkrmr <jerome.kramer@wanadoo.fr>
2016-11-18 14:11:24 +0100

    Ajout de variables de configuration

---
+++ config.sh
@@ -0,0 +1,7 @@
+jv_pg_dz_ip="127.0.0.1:8080"              # IP:port du serveur HTTP de domoticz
+jv_pg_dz_inter_MC="Mode chauffage"        # Nom de l'interrupteur gérant le mode de chauffage
+jv_pg_dz_NC_auto=10                       # Niveau de l'interrupteur pour le mode de chauffage AUTO
+jv_pg_dz_NC_eco=20                        # Niveau de l'interrupteur pour le mode de chauffage ECO
+jv_pg_dz_NC_confort=30                    # Niveau de l'interrupteur pour le mode de chauffage CONFORT
+jv_pg_dz_NC_manuel=40                     # Niveau de l'interrupteur pour le mode de chauffage MANUEL
+
